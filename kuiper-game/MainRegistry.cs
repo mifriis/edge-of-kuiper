b2b9018772770c37f1d47a622bf434b387286e6c
Alex W. Carlsen <alekcarlsen@gmail.com>
2021-09-19 00:12:00 +0200

    missing registry

--- kuiper-game/MainRegistry.cs
+++ kuiper-game/MainRegistry.cs
@@ -1,5 +1,6 @@
 using Kuiper.Services;
 using Kuiper.Systems;
+using Kuiper.Systems.CommandInfrastructure;
 using Lamar;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +13,7 @@ namespace Kuiper
             this.AddHostedService<MainLoopWorker>();
             this.AddSingleton<ICaptainService, CaptainService>();
             this.AddSingleton<ICaptainsConsole, CaptainsConsole>();
+            this.AddSingleton<ICommandParser, ConsoleCommandParser>();
             Scan((_) =>
             {
                 _.AssemblyContainingType<IConsoleCommand>();
