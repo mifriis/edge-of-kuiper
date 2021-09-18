4801400a8e85efd3363d845f6c3163647ee3d212
Alex W. Carlsen <alekcarlsen@gmail.com>
2021-09-19 00:24:48 +0200

    merge from master

--- kuiper-game/Systems/CaptainsConsole.cs
+++ kuiper-game/Systems/CaptainsConsole.cs
@@ -13,51 +13,18 @@ namespace Kuiper.Systems
         private readonly Captain _currentCaptain;
         private readonly ISolarSystemService _solarSystemService;
 
-        public CaptainsConsole(ICaptainService captainService, ISolarSystemService solarSystemService)
+        public CaptainsConsole(ICaptainService captainService, ISolarSystemService solarSystemService, ICommandParser commandParser)
         {
             _captainService = captainService;
             _solarSystemService = solarSystemService;
-
+            _commandParser = commandParser;
             _currentCaptain = _captainService.SetupCaptain();
             
         }
 
         public void ConsoleMapper(string input)
         {
-            switch (input)
-            {
-                case "help":
-                    Help();
-                    break;
-                case "save":
-                    Save();
-                    break;
-                case "time":
-                    CurrentTime();
-                    break;
-                case "ship description":
-                    Ship("description");
-                    break;
-                case "ship stats":
-                    Ship("stats");
-                    break;
-                case "ship location":
-                    Ship("location");
-                    break;
-                case "ship set course":
-                    Ship("set course");
-                    break;
-                case "test solarsystem":
-                    var earth = _solarSystemService.GetBody("Earth");
-                    var mercury = _solarSystemService.GetBody("Mercury");
-
-                    var dist = _solarSystemService.GetDistanceInAu(earth, mercury);
-                    ConsoleWriter.Write($"Distance is {dist} AU.");
-                    break;
-                default:
-                    ConsoleWriter.Write($"{input} not recognized. Try 'help' for list of commands");
-                    break;
-            }
+            _commandParser.ParseAndExecute(input);
         }
     }
 }
