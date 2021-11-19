f520696db24b4009bf4c4e6caf5227d1d027a454
Michael Friis <michael.nissen.thorup.friis@lego.com>
2021-11-19 22:28:55 +0100

    simple way to test the right services are called in an event

---
+++ kuiper-tests/Events/SetCourseEventShould.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using Moq;
+using Kuiper.Services;
+using Kuiper.Repositories;
+using Kuiper.Domain.CelestialBodies;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using Kuiper.Domain;
+using Kuiper.Systems.Events;
+using Lamar;
+
+namespace Kuiper.Tests.Unit.Services
+{
+    public class SetCourseEventShould
+    {
+        [Fact]
+        public void ExecuteTheRightServices()
+        {
+            //Arrange
+            var now = DateTime.Now;
+            var container = new Mock<IContainer>();
+            var shipService = new Mock<IShipService>();
+            var ship = new Ship("Johnny5", new ShipEngine(1,1,1,1),1) { FuelMass = 10, CurrentLocation = new CelestialBody() { Name = "Earth"}};
+            container.Setup(u => u.GetInstance<IShipService>()).Returns(shipService.Object);
+            shipService.SetupGet(u => u.Ship).Returns(ship);
+            var deltaVSpent = 100000;
+            var gameEvent = new SetCourseEvent() { DeltaVSpent = deltaVSpent };
+            
+            //Act
+            gameEvent.Execute(container.Object);
+
+            //Assert
+            shipService.Verify(x => x.FinalizeJourney(deltaVSpent), Times.Exactly(1));
+        }
+    }
+}
